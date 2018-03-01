96953a2f434d7f55e9c1064d86a9168d5b6fab26
Pav Sidhu <pav@pavsidhu.com>
2018-03-01 18:17:32 +0000

    Add Ride and Intersection

---
+++ src/Intersection.py
@@ -0,0 +1,7 @@
+class Intersection():
+    row = None
+    column = None
+
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
---
+++ src/Ride.py
@@ -0,0 +1,11 @@
+class Ride():
+    start_location = None
+    end_location = None
+    start_time = None
+    end_time = None
+
+    def __init__(self, start_location, end_location, start_time, finish_time):
+        self.start_location = start_location
+        self.end_location = end_location
+        self.start_time = start_time
+        self.end_time = end_Time
