3fb2c9b4eaa41bf6f2cdbe19886400ba69420c1c
axj714 <axj714@cs.bham.ac.uk>
2018-03-01 20:46:04 +0000

    asdf

--- src/Map.py
+++ src/Map.py
@@ -4,18 +4,11 @@ class Map():
     carList: list = []
     timeFrame: int = 0
 
-<<<<<<< HEAD
     def __init__(self, heightAndWidth, numberOfCars):
         self.heightAndWidth = heightAndWidth
         for i in numberOfCars:
             carList.append(Car(Intersection(0,0), 1)
     def nextTimeFrame():
-=======
-    def __init__(self, heightAndWidth):
-        self.heightAndWidth = heightAndWidth
-
-    def nextTimeFrame(self):
->>>>>>> 5153dce8ff2996600257aab3105b34298e3fc409
         self.timeFrame += 1
         for car in self.carList:
             car.move()
