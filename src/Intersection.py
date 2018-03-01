2a1defb8c92ea918abec117bfa9e31d6a736a29b
Jordan Mussi <jxm1099@cs.bham.ac.uk>
2018-03-01 19:37:14 +0000

    Type hinting & camelCase naming

--- src/Intersection.py
+++ src/Intersection.py
@@ -2,6 +2,12 @@ class Intersection():
     row = None
     column = None
 
-    def __init__(self, row, column):
+    def __init__(self, row: int, column: int) -> None:
+        assert row >= 0
+        assert column >= 0
+
         self.row = row
         self.column = column
+
+    def distanceTo(self, other: "Intersection") -> int:
+        return abs(self.row - other.row) + abs(self.column - other.column)
